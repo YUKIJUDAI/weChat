b839cbae2fa1739225d7df1d0810183eb570bd98
YUKIJUDAI <1787149082@qq.com>
2018-11-14 17:43:22 +0800

    init

--- app/taro/src/pages/index/index.js
+++ app/taro/src/pages/index/index.js
@@ -5,147 +5,147 @@ import { connect } from "@tarojs/redux";
 import "./index.less";
 
 @connect(({ counter }) => ({
-  counter
+    counter
 }))
 class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+    config = {
+        navigationBarTitleText: "首页"
     };
-  }
-  config = {
-    navigationBarTitleText: "首页"
-  };
-  componentDidMount() {
-    Taro.request({
-      url: "http://localhost/getIP",
-      method: "GET",
-      header: {
-        "content-type": "application/json"
-      }
-    }).then(res => this.setState({ value: res.data.ip_list[0] }));
-  }
+    componentDidMount() {
+        Taro.request({
+            url: "http://localhost/getIP",
+            method: "GET",
+            header: {
+                "content-type": "application/json"
+            }
+        }).then(res => this.setState({ value: res.data.ip_list[0] }));
+    }
 
-  render() {
-    return (
-      <View className="index">
-        <View style="text-align:center">
-          <Text>Taro</Text>
-        </View>
-        <View style="text-align:center">
-          文档地址：
-          <a href="https://nervjs.github.io/taro/" target="__block">
-            https://nervjs.github.io/taro/
-          </a>
-        </View>
-        <View style="text-align:center">
-          <table width="100%">
-            <thead>
-              <tr style="background-color: #F7F7F7">
-                <th width="30%">项目</th>
-                <th width="70%">说明</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>平台</td>
-                <td>移动端</td>
-              </tr>
-              <tr>
-                <td>移动端支持</td>
-                <td>暂无要求</td>
-              </tr>
-              <tr>
-                <td>是否开源</td>
-                <td>开源</td>
-              </tr>
-              <tr>
-                <td>编译产物</td>
-                <td>H5,微信小程序,百度小程序,支付宝小程序,React Native</td>
-              </tr>
-              <tr>
-                <td>文档</td>
-                <td>有，详细</td>
-              </tr>
-              <tr>
-                <td>依赖</td>
-                <td>react,tarojs/cli</td>
-              </tr>
-              <tr>
-                <td>路由</td>
-                <td>自带</td>
-              </tr>
-              <tr>
-                <td>状态管理</td>
-                <td>redux</td>
-              </tr>
-              <tr>
-                <td>ajax</td>
-                <td>自带Taro.request</td>
-              </tr>
-              <tr>
-                <td>国际化</td>
-                <td>无</td>
-              </tr>
-              <tr>
-                <td>自定义主题</td>
-                <td>第三方 UI 库</td>
-              </tr>
-              <tr>
-                <td>动画</td>
-                <td>canvas组件,css3</td>
-              </tr>
-              <tr>
-                <td>音频，视频</td>
-                <td>audio组件,video组件</td>
-              </tr>
-              <tr>
-                <td>Typescript</td>
-                <td>支持</td>
-              </tr>
-              <tr>
-                <td>异步编程</td>
-                <td>支持</td>
-              </tr>
-              <tr>
-                <td>组件</td>
-                <td>详见文档</td>
-              </tr>
-              <tr>
-                <td>工具</td>
-                <td>详见文档</td>
-              </tr>
-              <tr>
-                <td>图表</td>
-                <td>echarts-for-weixin</td>
-              </tr>
-              <tr>
-                <td>地图</td>
-                <td>百度地图，高德地图等</td>
-              </tr>
-              <tr>
-                <td>支付</td>
-                <td>微信支付,第三方支付平台</td>
-              </tr>
-              <tr>
-                <td>小程序第三方组件</td>
-                <td>支持</td>
-              </tr>
-              <tr>
-                <td>小程序原生代码支持</td>
-                <td>支持混写</td>
-              </tr>
-              <tr>
-                <td>调取微信接口例子</td>
-                <td> {this.state.value} </td>
-              </tr>
-            </tbody>
-          </table>
-        </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View className="index">
+                <View style="text-align:center">
+                    <Text>Taro</Text>
+                </View>
+                <View style="text-align:center">
+                    文档地址：
+                    <a href="https://nervjs.github.io/taro/" target="__block">
+                        https://nervjs.github.io/taro/
+                    </a>
+                </View>
+                <View style="text-align:center">
+                    <table width="100%">
+                        <thead>
+                            <tr style="background-color: #F7F7F7">
+                                <th width="30%">项目</th>
+                                <th width="70%">说明</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>平台</td>
+                                <td>移动端</td>
+                            </tr>
+                            <tr>
+                                <td>移动端支持</td>
+                                <td>暂无要求</td>
+                            </tr>
+                            <tr>
+                                <td>是否开源</td>
+                                <td>开源</td>
+                            </tr>
+                            <tr>
+                                <td>编译产物</td>
+                                <td>H5,微信小程序,百度小程序,支付宝小程序,React Native</td>
+                            </tr>
+                            <tr>
+                                <td>文档</td>
+                                <td>有，详细</td>
+                            </tr>
+                            <tr>
+                                <td>依赖</td>
+                                <td>react,tarojs/cli</td>
+                            </tr>
+                            <tr>
+                                <td>路由</td>
+                                <td>自带</td>
+                            </tr>
+                            <tr>
+                                <td>状态管理</td>
+                                <td>redux</td>
+                            </tr>
+                            <tr>
+                                <td>ajax</td>
+                                <td>自带Taro.request</td>
+                            </tr>
+                            <tr>
+                                <td>国际化</td>
+                                <td>无</td>
+                            </tr>
+                            <tr>
+                                <td>自定义主题</td>
+                                <td>第三方 UI 库</td>
+                            </tr>
+                            <tr>
+                                <td>动画</td>
+                                <td>canvas组件,css3</td>
+                            </tr>
+                            <tr>
+                                <td>音频，视频</td>
+                                <td>audio组件,video组件</td>
+                            </tr>
+                            <tr>
+                                <td>Typescript</td>
+                                <td>支持</td>
+                            </tr>
+                            <tr>
+                                <td>异步编程</td>
+                                <td>支持</td>
+                            </tr>
+                            <tr>
+                                <td>组件</td>
+                                <td>详见文档</td>
+                            </tr>
+                            <tr>
+                                <td>工具</td>
+                                <td>详见文档</td>
+                            </tr>
+                            <tr>
+                                <td>图表</td>
+                                <td>echarts-for-weixin</td>
+                            </tr>
+                            <tr>
+                                <td>地图</td>
+                                <td>百度地图，高德地图等</td>
+                            </tr>
+                            <tr>
+                                <td>支付</td>
+                                <td>微信支付,第三方支付平台</td>
+                            </tr>
+                            <tr>
+                                <td>小程序第三方组件</td>
+                                <td>支持</td>
+                            </tr>
+                            <tr>
+                                <td>小程序原生代码支持</td>
+                                <td>支持混写</td>
+                            </tr>
+                            <tr>
+                                <td>调取微信接口例子</td>
+                                <td> {this.state.value} </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </View>
+            </View>
+        );
+    }
 }
 
 export default Index;
--- service/config.js
+++ service/config.js
@@ -4,5 +4,5 @@ module.exports = {
     AppSecret: "165a7e6bbd5cdee33b3e908feef20cc6",
     // access_token的有效期目前为2个小时，需定时刷新，重复获取将导致上次获取的access_token失效。
     access_token:
-        "15_vOi1u7hjWjp8nIcsMUCa4H2E_ZZm0mB34xJL_cc37a4fDGvXt9KbNnR-wHGsYkCuKoeTxTE0Rxklc0gU0Qy6RYDd3xchGIGwOa4a53r4zlLJKnL4SIv1AAUFR41HJ1DOaBL0dQi1lpqAz9UOJVNgAJAAME"
+        "15_j-APjY6o8-RBtDcDJtHXl36OxpiMejQ9-CD9ltppV7-JNcv16PP4KBPPtNFbvzWsrE-AS3bOcHb0nweig4sDV-J6RhX-2bjf38nwvsQ7Eb0SEzqpCzLyGzJPIWFuPmNBTgmbVGS36gJD5L08FZKjAFAICN"
 };
